c28deccad66bbd2c100f0fa291ad3203a6635b07
sylwek2531 <sylwester2531@gmail.com>
2018-05-17 10:07:19 +0200

    add config file, grunt , add sass preprocessor, create giignore file

---
+++ Gruntfile.js
@@ -0,0 +1,56 @@
+module.exports = function (grunt) {
+
+    var serveStatic = require('serve-static');
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+
+
+    grunt.initConfig({
+        watch: {
+            livereload: {
+                options: {
+                    livereload: '<%= connect.options.livereload %>'
+                },
+                files: [
+                    'app/**/*.html',
+                    'app/**/*.js',
+                    'app/**/*.css'
+                ]
+            }
+        },
+        connect: {
+            options: {
+                port: 9000,
+                livereload: 35729,
+                hostname: 'localhost'
+            },
+            livereload: {
+                options: {
+                    open: true,
+                    middleware: function (connect) {
+                        return [connect().use('/bower_components', serveStatic('./bower_components')), serveStatic('app')];
+                    }
+                }
+            }
+        },
+        sass: {                              // Task
+            dist: {                            // Target
+                options: {                       // Target options
+                    style: 'expanded',
+                    noCache: true
+                },
+                files: {                         // Dictionary of files
+                    'app/style.css': 'app/style.scss'     // 'destination': 'source'
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('serve', function () {
+        grunt.task.run(['connect:livereload', 'watch']);
+    });
+
+    grunt.registerTask('default', ['serve', 'sass']);
+};
